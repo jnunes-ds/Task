0b5c7a85ac6aef5f912470e0c9ffee7ac7a96ea4
Júnior Nunes <denisonprobable13@gmail.com>
2021-04-16 19:33:15 -0300

    Starting to style the Task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default props => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>{props.desc}</Text>
             <Text>{props.estimateAt + ""}</Text>
             <Text>{props.done + ""}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: '#AAA',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+});
